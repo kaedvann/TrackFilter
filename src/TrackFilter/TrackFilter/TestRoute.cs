50980a88376ef2d0d2eb7fb658e154c39fa8bcb5
Rostislav Listerenko <r.listerenko@gmail.com>
2015-04-18 22:08:17 +0300

    Контрол с картой и примером маршрута.

---
+++ src/TrackFilter/TrackFilter/TestRoute.cs
@@ -0,0 +1,9 @@
+﻿using GMap.NET;
+
+namespace TrackFilter
+{
+    class TestRoute
+    {
+        public static readonly PointLatLng[] Points = { new PointLatLng(55.775574, 37.535989), new PointLatLng(55.775152, 37.537749), new PointLatLng(55.773668, 37.537127), new PointLatLng(55.772630, 37.541740), new PointLatLng(55.773414, 37.545431) };
+    }
+}
---
+++ src/TrackFilter/TrackFilter/Views/MapControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
+
+namespace TrackFilter.Views
+{
+    /// <summary>
+    /// Interaction logic for MapControl.xaml
+    /// </summary>
+    public partial class MapControl
+    {
+        private  readonly PointLatLng _moscow = new PointLatLng(55.750303, 37.622656);
+
+        public MapControl()
+        {
+            InitializeComponent();
+            Map.MapProvider = GMapProviders.GoogleMap;
+            GMapProvider.WebProxy = null;
+            Map.MouseWheelZoomType = MouseWheelZoomType.MousePositionWithoutCenter;
+            Map.CanDragMap = true;
+            Map.Manager.Mode = AccessMode.ServerAndCache;
+            Map.Zoom = 14;
+            Map.Position = _moscow;
+        }
+
+        public void RenderTrack(IEnumerable<PointLatLng> points)
+        {
+            var marker = new GMapRoute(points);
+            
+            Map.Markers.Add(marker);
+        }
+    }
+}
